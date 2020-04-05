9aef8aa785067682f737be116c31f3693729213f
Ashok Gudise <ashok.gudise@gmail.com>
2020-04-05 03:22:10 -0400

    first commit

---
+++ src/main/java/com/reactiveapps/demos/Application.java
@@ -0,0 +1,27 @@
+package com.reactiveapps.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ https://able.bio/DavidLandup/real-time-event-streaming-using-spring-webflux--156loir
+
+ https://itnext.io/ngrx-store-and-5-silly-mistakes-780ae2f961d0
+
+ Material Design
+ https://timdeschryver.dev/blog/start-using-ngrx-effects-for-this
+
+ https://www.techiediaries.com/angular/angular-9-8-tutorial-by-example-rest-crud-apis-http-get-requests-with-httpclient/
+
+
+ https://raymondhlee.wordpress.com/2019/11/23/a-reactive-stack-with-spring-boot-kafka-and-angular/
+ **/
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/reactiveapps/demos/config/CorsGlobalConfiguration.java
@@ -0,0 +1,18 @@
+package com.reactiveapps.demos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET")
+                .maxAge(3600);
+    }
+}
